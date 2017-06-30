8c445f8a9792cf9d91d86598121a76fb50baccfa
Nick Cresner <nickcresner@gmail.com>
2017-06-30 17:19:59 +0100

    updated views controller

--- src/js/controllers/posts.js
+++ src/js/controllers/posts.js
@@ -10,3 +10,52 @@ function PostsIndexCtrl(Post, $auth){
   }
   postsIndex();
 }
+
+PostsNewCtrl.$inject = ['Post', '$state'];
+function PostsNewCtrl(Post, $state) {
+  const vm = this;
+  vm.post = {};
+
+  function postsCreate() {
+    if (vm.postForm.$valid) {
+      Post
+        .save(vm.post)
+        .$promise
+        .then(() => $state.go('postsIndex'));
+    }
+  }
+
+  vm.create = postsCreate;
+}
+
+PostsShowCtrl.$inject = ['Post', '$stateParams', '$state'];
+function PostsShowCtrl(Post, $stateParams, $state) {
+  const vm = this;
+
+  vm.post = Post.get($stateParams);
+
+  function postsDelete() {
+    vm.post
+      .$remove()
+      .then(() => $state.go('postsIndex'));
+  }
+
+  vm.delete = postsDelete;
+}
+
+PostsEditCtrl.$inject = ['Post', '$stateParams', '$state'];
+function PostsEditCtrl(Post, $stateParams, $state) {
+  const vm = this;
+
+  vm.post = Post.get($stateParams);
+
+  function postsUpdate() {
+    if (vm.postForm.$valid) {
+      vm.post
+        .$update()
+        .then(() => $state.go('postsShow', $stateParams));
+    }
+  }
+
+  vm.update = postsUpdate;
+}
