d4d542881a8d2b968b5d48be741137455ed2e5e1
larmflower <lnarosetta@gmail.com>
2017-06-30 16:16:35 +0100

    commit

--- config/routes.js
+++ config/routes.js
@@ -9,11 +9,11 @@ router.route('/posts')
   .get(postsController.indexPostsRoute)
   .post(postsController.createPostsRoute);
 
-router.route('posts/:id')
+router.route('/posts/:id')
   .get(postsController.showPostsRoute)
   .put(postsController.updatePostsRoute)
   .delete(postsController.deletePostsRoute);
-  
+
 router.route('/foodbanks')
   .get(foodBanksController.indexFoodBanksRoute)
   .post(foodBanksController.createFoodBanksRoute);
@@ -22,6 +22,7 @@ router.route('/foodbanks/:id')
   .get(foodBanksController.showFoodBanksRoute)
   .put(foodBanksController.updateFoodBanksRoute)
   .delete(foodBanksController.deleteFoodBanksRoute);
+
 
 router.route('/users/:id')
   .get(usersController.showUsersRoute)
---
+++ test/api/posts_test.js
@@ -0,0 +1,8 @@
+// require('../helper');
+//
+// const Shoe        = require('../../models/shoe');
+// const User        = require('../../models/user');
+// const jwt         = require('jsonwebtoken');
+// const { secret }  = require('../../config/environment');
+//
+// let token;
