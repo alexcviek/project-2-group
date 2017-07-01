f4d2d00e99e2c1c99808131c992bb78c35032341
Alex Cwiek <a.i.cwiek@gmail.com>
2017-07-01 19:45:58 +0100

    adding parts for oauth

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -1,1 +1,109 @@
-//for facebook and instagram
+//for facebook and instagram and github
+const rp = require('request-promise');
+const oauth = require('../config/oauth');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/environment');
+
+function github(req, res, next) {
+  return rp({
+    method: 'POST',
+    url: oauth.github.accessTokenURL,
+    qs: {
+      client_id: oauth.github.clientId,
+      client_secret: oauth.github.clientSecret,
+      code: req.body.code
+    },
+    json: true
+  })
+  .then((token) => {
+    return rp({
+      method: 'GET',
+      url: oauth.github.profileURL,
+      qs: token,
+      json: true,
+      headers: {
+        'User-Agent': 'Request-Promise'
+      }
+    });
+  })
+  .then((profile) => {
+    return User.findOne({ $or: [{ githubId: profile.id },{ email: profile.email }] })
+      .then((user) => {
+        if(!user) {
+          user = new User({
+            username: profile.login,
+            email: profile.email
+          });
+        }
+
+        user.githubId = profile.id;
+        user.image = profile.avatar_url;
+        return user.save();
+      });
+  })
+  .then((user) => {
+    console.log(user);
+    const payload = { userId: user.id };
+    const token   = jwt.sign(payload, secret, { expiresIn: '1hr' });
+
+    return res.json({
+      token,
+      message: `Welcome back, ${user.username}`
+    });
+  })
+  .catch(next);
+}
+
+function facebook(req, res, next) {
+  return rp({
+    method: 'GET',
+    url: oauth.facebook.accessTokenUrl,
+    qs: {
+      client_id: oauth.facebook.clientId,
+      redirect_uri: oauth.facebook.redirectUri,
+      client_secret: oauth.facebook.clientSecret,
+      code: req.body.code
+    },
+    json: true
+  })
+  .then((token) => {
+    return rp({
+      method: 'GET',
+      url: 'https://graph.facebook.com/v2.5/me?fields=id,name,email,picture.height(300)',
+      qs: token,
+      json: true
+    });
+  })
+  .then((profile) => {
+    console.log(profile);
+    return User.findOne({$or: [{facebookId: profile.id }, { email: profile.email }]})
+    .then((user) => {
+      if(!user){
+        user = new User({
+          username: profile.name,
+          email: profile.email
+        });
+      }
+      user.facebookId = profile.id;
+      user.image = profile.picture.data.url;
+      return user.save();
+    });
+  })
+  .then((user) => {
+    console.log(user);
+    const payload = { userId: user.id };
+    const token   = jwt.sign(payload, secret, { expiresIn: '1hr' });
+
+    return res.json({
+      token,
+      message: `Welcome back, ${user.username}`
+    });
+  })
+  .catch(next);
+}
+
+module.exports = {
+  github,
+  facebook
+};
