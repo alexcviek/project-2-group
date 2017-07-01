eaa5ec20e7337cc0ee35e32504edcd2512969661
Nick Cresner <nickcresner@gmail.com>
2017-07-01 21:33:43 +0100

    updated user views

---
+++ src/js/controllers/users.js
@@ -0,0 +1,18 @@
+angular
+  .module('sausageApp')
+  .controller('ProfileCtrl', ProfileCtrl);
+
+ProfileCtrl.$inject = ['$auth', 'User', '$state'];
+function ProfileCtrl($auth, User, $state) {
+  const vm = this;
+  const { userId } = $auth.getPayload();
+
+  if(userId) vm.user = User.get({ id: userId });
+
+  function logout() {
+    $auth.logout();
+    $state.go('login');
+  }
+
+  vm.logout = logout;
+}
