4483fc37fabf36fae641645370f4c938883afee0
Alex Cwiek <a.i.cwiek@gmail.com>
2017-07-02 15:18:54 +0100

    added usersshow controller

--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -1,16 +1,18 @@
 angular
 .module('sausageApp')
-.controller('ProfileCtrl', ProfileCtrl)
-.controller('UsersEditCtrl', UsersEditCtrl);
+.controller('UsersEditCtrl', UsersEditCtrl)
+.controller('UsersShowCtrl', UsersShowCtrl);
 
-
-ProfileCtrl.$inject = ['$auth', 'User'];
-function ProfileCtrl($auth, User) {
+UsersShowCtrl.$inject = ['User', '$stateParams', '$state'];
+function UsersShowCtrl(User, $stateParams, $state) {
   const vm = this;
-  const { userId } = $auth.getPayload();
-
-  if(userId) vm.user = User.get({ id: userId });
-
+  vm.post = User.get($stateParams);
+  function userDelete() {
+    vm.post
+    .$remove()
+    .then(() => $state.go('home'));
+  }
+  vm.delete = userDelete;
 }
 
 UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
@@ -29,10 +31,4 @@ function UsersEditCtrl(User, $stateParams, $state) {
 
   vm.update = usersUpdate;
 
-  // function logout() {
-  //   $auth.logout();
-  //   $state.go('login');
-  // }
-  //
-  // vm.logout = logout;
 }
