0407804a66be4b48b218e3fbf44833b8e67f111b
Nick Cresner <nickcresner@gmail.com>
2017-07-02 09:38:20 +0100

    updated user controller and new user form

--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -9,6 +9,22 @@ function ProfileCtrl($auth, User, $state) {
 
   if(userId) vm.user = User.get({ id: userId });
 
+  UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
+  function UsersEditCtrl(User, $stateParams, $state) {
+    const vm = this;
+
+    vm.post = User.get($stateParams);
+
+    function usersUpdate() {
+      if (vm.userForm.$valid) {
+        vm.user
+          .$update()
+          .then(() => $state.go('usersShow', $stateParams));
+      }
+    }
+
+    vm.update = usersUpdate;
+
   function logout() {
     $auth.logout();
     $state.go('login');
