06928675253c255783fa42fb4785c099d5c15665
Nick Cresner <nickcresner@gmail.com>
2017-07-02 18:46:41 +0100

    editied user views

--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -1,34 +1,37 @@
 angular
 .module('sausageApp')
-.controller('UsersEditCtrl', UsersEditCtrl)
-.controller('UsersShowCtrl', UsersShowCtrl);
+.controller('ProfileCtrl', ProfileCtrl);
 
-UsersShowCtrl.$inject = ['User', '$stateParams', '$state'];
-function UsersShowCtrl(User, $stateParams, $state) {
+
+ProfileCtrl.$inject = ['$auth', 'User'];
+function ProfileCtrl($auth, User) {
   const vm = this;
-  vm.user = User.get($stateParams);
-  function userDelete() {
-    vm.post
-    .$remove()
-    .then(() => $state.go('home'));
-  }
-  vm.delete = userDelete;
-}
+  const { userId } = $auth.getPayload();
 
-UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
-function UsersEditCtrl(User, $stateParams, $state) {
-  const vm = this;
+  if(userId) vm.user = User.get({ id: userId });
 
-  vm.post = User.get($stateParams);
 
-  function usersUpdate() {
-    if (vm.userForm.$valid) {
-      vm.user
-      .$update()
-      .then(() => $state.go('usersShow', $stateParams));
+  UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
+  function UsersEditCtrl(User, $stateParams, $state) {
+    const vm = this;
+
+    vm.post = User.get($stateParams);
+
+    function usersUpdate() {
+      if (vm.userForm.$valid) {
+        vm.user
+        .$update()
+        .then(() => $state.go('usersShow', $stateParams));
+      }
     }
+
+    vm.update = usersUpdate;
+
+    function logout() {
+      $auth.logout();
+      $state.go('login');
+    }
+
+    vm.logout = logout;
   }
-
-  vm.update = usersUpdate;
-
 }
