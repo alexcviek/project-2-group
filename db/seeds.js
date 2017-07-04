e3d880d45a477c37dc7e97077a223a243976f288
larmflower <lnarosetta@gmail.com>
2017-07-04 16:11:07 +0100

    more items more foodbanks in seeds

--- db/seeds.js
+++ db/seeds.js
@@ -60,6 +60,24 @@ User
         url: 'http://fatcat.co.uk',
         image: 'http://i.dailymail.co.uk/i/pix/2016/10/04/17/39191F2600000578-3821775-image-a-40_1475597321353.jpg',
         createdBy: users[1]
+      },{
+        name: 'Hackney Food Bank',
+        location: { lat: 51.5254708,
+          lng: -0.099727
+        },
+        type: 'Food Bank',
+        url: 'https://www.google.co.uk/url?sa=t&rct=j&url=http%3A%2F%2Fwww.hackney.foodbank.org.uk%2F&source=maps&ei=VaxbWYiGDcqRgAaxjbnoAg%3A203&cd=1&usg=AFQjCNG-a7mA_QZncjlS0V667Iv5DNIP6Q&ved=1i%3A1%2Ct%3A3443%2Ce%3A0%2Cp%3AVaxbWYiGDcqRgAaxjbnoAg%3A203',
+        image: 'http://www.theology-centre.org.uk/wp-content/uploads/2013/08/Tesco-Foodbank-Collection-July-2013.jpg',
+        createdBy: users[2]
+      },{
+        name: 'The Trussell Trust Food Bank',
+        location: { lat: 51.5254708,
+          lng: -0.099727
+        },
+        type: 'Food Bank',
+        url: 'http://trusselltrust.org',
+        image: 'https://leftfootforward.org/images/2014/02/Food-banksj.jpg',
+        createdBy: users[2]
       }])
       .then((foodBanks) => {
         console.log(`${foodBanks.length} food banks created!`);
@@ -95,6 +113,48 @@ User
             title: 'Loads of food',
             separable: false,
             image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Food_into_a_refrigerator_-_20111002.jpg',
+            location: { lat: -51.521610,
+              lng: -0.059307
+            },
+            item: [{
+              name: 'Eggs',
+              type: 'food',
+              perishable: true,
+              image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Food_into_a_refrigerator_-_20111002.jpg',
+              claimed: false
+            },{
+              name: 'Sausage',
+              type: 'food',
+              perishable: true,
+              image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Food_into_a_refrigerator_-_20111002.jpg',
+              claimed: false
+            }],
+            createdBy: users[2]
+          },{
+            title: 'Loads of food',
+            separable: false,
+            image: 'https://images.unsplash.com/photo-1418669112725-fb499fb61127?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
+            location: { lat: -51.521610,
+              lng: -0.059307
+            },
+            item: [{
+              name: 'Eggs',
+              type: 'food',
+              perishable: true,
+              image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Food_into_a_refrigerator_-_20111002.jpg',
+              claimed: false
+            },{
+              name: 'Sausage',
+              type: 'food',
+              perishable: true,
+              image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Food_into_a_refrigerator_-_20111002.jpg',
+              claimed: false
+            }],
+            createdBy: users[2]
+          },{
+            title: 'Loads of food',
+            separable: false,
+            image: 'https://images.unsplash.com/photo-1417976528056-3c9bbbb5456c?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
             location: { lat: -51.521610,
               lng: -0.059307
             },
